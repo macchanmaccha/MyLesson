952b662a49c90591f28a8fccda9338e1c9c87a9d
may <queenqueenqueen13@gmail.com>
2022-06-10 23:57:26 +0900

    関数の課題3の練習

--- tryDivisorPrimeApp.playground/Contents.swift
+++ tryDivisorPrimeApp.playground/Contents.swift
@@ -6,16 +6,17 @@ func primeNumber(inputNumbers: Int) -> [Int] {
     // 割り切れる数が2つしかない時
     // 格納する箱
     var countPrimeNumber: [Int] = []
+    var counter: Int = 0
     for inputNumber in 1..<inputNumbers {
         for number in 1..<inputNumber {
             if  inputNumber % number == 0 {
-                countPrimeNumber.append(inputNumber)
-                if countPrimeNumber.count == 2 {
-                    print(countPrimeNumber)
-                }
+                counter += 1
             } // ifここまで
         } // forここまで
     } // forここまで
+    if counter == 2 {
+        print(countPrimeNumber)
+    }
     return countPrimeNumber
 } // primeNumberここまで
 
